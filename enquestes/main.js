595fe0c6d683d38baea866cbfdd1b5f5d1047e82
Koala Code <k0a1ac0d3@gmail.com>
2024-04-12 16:22:57 +0200

    Fixed: -Obtenir arxius method. -Files names. -More routes.

--- enquestes/main.js
+++ enquestes/main.js
@@ -1,20 +1,27 @@
-// Port PostgreSQL 16 en local: 5433
-
 // Funció per obtenir una llista dels arxius d'una carpeta
 async function obtenirArxius(carpeta) {
-    const resposta = await fetch(carpeta);
-    const text = await resposta.text();
-    const parser = new DOMParser();
-    const documentHTML = parser.parseFromString(text, 'text/html');
-    const enllacos = documentHTML.querySelectorAll('a');
-    return Array.from(enllacos)
-        .map(enllac => enllac.getAttribute('href'))
-        .filter(enllac => enllac.endsWith('.json'));
+    try {
+        const resposta = await fetch(carpeta);
+        const text = await resposta.text();
+        const parser = new DOMParser();
+        const documentHTML = parser.parseFromString(text, 'text/html');
+        const enllacos = documentHTML.querySelectorAll('a');
+        return Array.from(enllacos)
+            .map(enllac => {
+                const rutaArxiu = enllac.getAttribute('href');
+                // Només retorna el nom del fitxer, sense cap ruta prèvia
+                return rutaArxiu.substring(rutaArxiu.lastIndexOf('/') + 1);
+            })
+            .filter(enllac => enllac.endsWith('.json'));
+    } catch (error) {
+        console.error('Error en obtenir els arxius:', error);
+        return [];
+    }
 }
 
 // Funció per carregar la llista d'enquestes disponibles
 async function carregarLlistaEnquestes() {
-    const carpetaPreguntes = './preguntes/JSON';
+    const carpetaPreguntes = '/enquestes/preguntes/JSON';
 
     // Obté una llista dels arxius JSON de la carpeta "preguntes/JSON"
     const arxius = await obtenirArxius(carpetaPreguntes);
@@ -29,7 +36,7 @@ async function carregarLlistaEnquestes() {
         // Crea un element d'enllaç per cada arxiu JSON
         const enllaç = document.createElement('a');
         enllaç.textContent = nomEnquesta;
-        enllaç.href = `preguntes/index.html?arxiu=${arxiu}&nomEnquesta=${encodeURIComponent(nomEnquesta)}`; // Passa el nom de l'arxiu com a paràmetre a través de la URL
+        enllaç.href = `preguntes/formulari.html?arxiu=${arxiu}&nomEnquesta=${encodeURIComponent(nomEnquesta)}`; // Passa el nom de l'arxiu com a paràmetre a través de la URL
         enllaç.classList.add('enllaç-enquesta');
         
         // Crea un element d'element de llista per mostrar l'enllaç
@@ -44,7 +51,6 @@ async function carregarLlistaEnquestes() {
 // Carrega la llista d'enquestes quan la pàgina s'ha carregat completament
 document.addEventListener('DOMContentLoaded', carregarLlistaEnquestes);
 
-
 // Per el formulari de les preguntes de l'enquesta
 async function llegirDades(rutaArxiu) {
     try {
@@ -57,21 +63,21 @@ async function llegirDades(rutaArxiu) {
     }
 }
 
-// Declaración de variables globales
-let preguntas; // Almacenará todas las preguntas
-let preguntaActual = 0; // Variable para seguir la pregunta actual
+// Declaració de variables globals
+let preguntas; // Almacenarà totes les preguntes
+let preguntaActual = 0; // Variable per seguir la pregunta actual
 
-// Función para cargar las preguntas y mostrar la primera pregunta
-async function cargarPreguntas() {
-    const rutaArxiu = obtenerParametroUrl('arxiu');
-    const nomEnquesta = obtenerParametroUrl('nomEnquesta');
+// Funció per carregar les preguntes i mostrar la primera pregunta
+async function carregarPreguntes() {
+    const rutaArxiu = obtenirParametreUrl('arxiu');
+    const nomEnquesta = obtenirParametreUrl('nomEnquesta');
     const titolEnquesta = document.createElement('h2');
     const title = document.getElementById('title');
 
     preguntas = await llegirDades(rutaArxiu);
 
     if (!preguntas || preguntas.length === 0) {
-        console.error('No se pudieron cargar las preguntas.');
+        console.error('No es van poder carregar les preguntes.');
         return;
     }
 
@@ -82,10 +88,10 @@ async function cargarPreguntas() {
     mostrarPregunta();
 }
 
-// Función para mostrar la pregunta actual
+// Funció per mostrar la pregunta actual
 function mostrarPregunta() {
     const formulariDiv = document.getElementById('formulari');
-    formulariDiv.innerHTML = ''; // Limpiar el formulario antes de mostrar la nueva pregunta
+    formulariDiv.innerHTML = ''; // Netegem el formulari abans de mostrar la nova pregunta
 
     const pregunta = preguntas[preguntaActual];
     const label = document.createElement('label');
@@ -101,79 +107,78 @@ function mostrarPregunta() {
         if (event.key === 'Enter') {
             event.preventDefault(); // Evita l'enviament del formulari
             if (preguntaActual < preguntas.length - 1) {
-                siguientePregunta(); // Passa a la següent pregunta
+                seguentPregunta(); // Passa a la següent pregunta
             } else {
                 enviarFormulari(); // Envia el formulari
             }
         }
     });
 
-    if (pregunta.opcions) { // Si es un campo select
+    if (pregunta.opcions) { // Si és un camp selecte
         const select = document.createElement('select');
-        pregunta.opcions.forEach(opcion => {
+        pregunta.opcions.forEach(opcio => {
             const option = document.createElement('option');
-            option.value = opcion;
-            option.textContent = opcion;
+            option.value = opcio;
+            option.textContent = opcio;
             select.appendChild(option);
         });
         formulariDiv.appendChild(label);
         formulariDiv.appendChild(select);
-    } else { // Si no es un campo select
+    } else { // Si no és un camp selecte
         formulariDiv.appendChild(label);
         formulariDiv.appendChild(input);
     }
 
-    // Crear el botón de "Siguiente" o "Enviar"
-    const botonSiguiente = document.createElement('button');
+    // Crear el botó de "Següent" o "Enviar"
+    const botoSeguent = document.createElement('button');
     if (preguntaActual < preguntas.length - 1) {
-        botonSiguiente.textContent = 'Siguiente';
-        botonSiguiente.addEventListener('click', siguientePregunta);
-        botonSiguiente.classList.add('form-buttons');
+        botoSeguent.textContent = 'Següent';
+        botoSeguent.addEventListener('click', seguentPregunta);
+        botoSeguent.classList.add('form-buttons');
     } else {
-        botonSiguiente.textContent = 'Enviar';
-        botonSiguiente.addEventListener('click', enviarFormulari);
-        botonSiguiente.classList.add('submit-button');
+        botoSeguent.textContent = 'Enviar';
+        botoSeguent.addEventListener('click', enviarFormulari);
+        botoSeguent.classList.add('submit-button');
     }
     
     const botoDiv = document.createElement('div');
 
-    // Crear el botón de "Anterior" si no es la primera pregunta
+    // Crear el botó de "Anterior" si no és la primera pregunta
     if (preguntaActual > 0) {
-        const botonAnterior = document.createElement('button');
-        botonAnterior.textContent = 'Anterior';
-        botonAnterior.addEventListener('click', anteriorPregunta);
-        botonAnterior.classList.add('form-buttons');
+        const botoAnterior = document.createElement('button');
+        botoAnterior.textContent = 'Anterior';
+        botoAnterior.addEventListener('click', anteriorPregunta);
+        botoAnterior.classList.add('form-buttons');
         botoDiv.classList.remove('right-div');
-        botoDiv.appendChild(botonAnterior);
+        botoDiv.appendChild(botoAnterior);
         botoDiv.classList.add('space-div');
         formulariDiv.appendChild(botoDiv);
     }
 
-    // Afegir el botó de "Siguiente" o "Enviar"
-    botoDiv.appendChild(botonSiguiente);
+    // Afegir el botó de "Següent" o "Enviar"
+    botoDiv.appendChild(botoSeguent);
     botoDiv.classList.add('space-div');
 
     // Afegir botoDiv al formulari
     formulariDiv.appendChild(botoDiv);
 }
 
-
-// Función para pasar a la siguiente pregunta
-function siguientePregunta() {
+// Funció per passar a la següent pregunta
+function seguentPregunta() {
     preguntaActual++;
     mostrarPregunta();
 }
 
-// Función para volver a la pregunta anterior
+// Funció per tornar a la pregunta anterior
 function anteriorPregunta() {
     preguntaActual--;
     mostrarPregunta();
 }
 
-// Funció per enviar el formulari (quan s'arriba a la última pregunta)
+// Funció per enviar el formulari (quan s'arriba a l'última pregunta)
 async function enviarFormulari() {
     try {
-        // Obté les respostes del formulari
+        // Obtenir les respostes del formulari
         const formulari = document.getElementById('formulari');
         if (!formulari || formulari.nodeName !== 'FORM') {
             console.error('L\'element del formulari no és vàlid');
@@ -187,7 +192,7 @@ async function enviarFormulari() {
             answers: [] // Array per emmagatzemar les respostes
         };
 
-        // Recorre totes les claus i valors del formulari per construir les respostes
+        // Recórrer totes les claus i valors del formulari per construir les respostes
         formData.forEach((valor, clau) => {
             // Afegir cada resposta al format d'objecte esperat pel backend
             const resposta = {
@@ -198,7 +203,7 @@ async function enviarFormulari() {
             dades.answers.push(resposta);
         });
 
-        // Envia les dades al servidor mitjançant una crida POST
+        // Enviar les dades al servidor mitjançant una crida POST
         const response = await fetch('http://10.2.246.94:8001/api/preguntes', {
             method: 'POST',
             headers: {
@@ -223,11 +228,11 @@ document.getElementById('formulari').addEventListener('submit', function(event) 
     enviarFormulari();
 });
 
-// Función para obtener un parámetro de la URL por su nombre
-function obtenerParametroUrl(nombre) {
+// Funció per obtenir un paràmetre de la URL pel seu nom
+function obtenirParametreUrl(nom) {
     const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(nombre);
+    return urlParams.get(nom);
 }
 
-// Crear el formulario al cargar la página
-window.onload = cargarPreguntas;
+// Crear el formulari en carregar la pàgina
+window.onload = carregarPreguntes;
